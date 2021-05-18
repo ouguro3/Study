688ee60bfc59dbf6e430a3fcb6065e042f62e615
Lee-HB <dkdldspd3@gmail.com>
2021-05-18 10:26:52 +0900

    Python

---
+++ Python_Basic/02_dataType/data1.py
@@ -0,0 +1,19 @@
+# 진수 변환 함수
+
+# 2진수로 변환
+print('2진수로 변환')
+print(bin(11))
+print(bin(0o11))
+print(bin(0x11))
+
+# 8진수로 변환
+print('8진수로 변환')
+print(oct(11))
+print(oct(0o11))
+print(oct(0x11))
+
+# 16진수로 변환
+print('16진수로 변환')
+print(hex(11))
+print(hex(0o11))
+print(hex(0x11))
