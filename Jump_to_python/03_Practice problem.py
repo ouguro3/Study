5594e7dc60d8ccd6a8e7e355dcf41ab5105728e7
Lee-HB <dkdldspd3@gmail.com>
2021-05-23 23:50:58 +0900

    Python

---
+++ Jump_to_python/03_Practice problem.py
@@ -0,0 +1,22 @@
+# Q3. while문을 이용하여 1 부터 1000의 자연수 중 3의 배수의 합을 구하라
+# a = 0
+# total = 0
+# while a <= 1000:
+#     a += 1
+#     if a % 3 == 0:
+#         total += a
+# print(total)
+
+# Q4. for문을 사용해 1부터 100까지의 숫자를 출력해보자
+# for i in range(1,1001):
+#     print(i)
+
+# Q5. A 학급에 총 10명의 학생이 있다.  이들의 중간고사 점수는 다음과 같다
+# [70,60,55,75,95,90,80,80,85,100]
+# for문을 사용하여 A 학급의 평균 점수를 구해보자
+# score = [70,60,55,75,95,90,80,80,85,100]
+# total = 0
+# for i in score:
+#     total += i
+# aver = total/(len(score))
+# print(aver)
