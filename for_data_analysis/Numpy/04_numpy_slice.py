d4773eebde8069efbf1d0bf4886aa0455769e031
Lee-HB <dkdldspd3@gmail.com>
2021-06-02 22:21:52 +0900

    Python

--- for_data_analysis/Numpy/04_numpy_slice.py
+++ for_data_analysis/Numpy/04_numpy_slice.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # 배열의 슬라이싱
-b = np.array([10,20,30,40,50])
+b = np.array([0,10,20,30,40,50])
 b1 = b[1:4]
 print(b1)
 
@@ -14,3 +14,25 @@ print(b1)
 # 원소 변경
 b1 = b[2:5] = np.array([25,35,45])
 print(b1)
+
+b[3:6] = 60
+print(b)
+
+# 2차원 배열의 슬라이싱
+b = np.arange(10,100,10).reshape(3,3)
+print(b)
+
+b2 = b[1:3,1:3]
+print(b2)
+
+# 일부 생략
+b2 = b[:3,1:]
+print(b2)
+
+# 행 지정 열 슬라이싱
+b2 = b[1][0:2]
+print(b2)
+
+# 슬라이싱 된 배열에 값 지정
+b[0:2, 1:3] = np.array([[25,35],[55,65]])
+print(b)
