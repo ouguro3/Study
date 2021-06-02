34506f04dd029540e89b6aff72f268d0d8574f34
Lee-HB <dkdldspd3@gmail.com>
2021-06-02 22:27:25 +0900

    Python

--- for_data_analysis/Numpy/03_numpy_index.py
+++ for_data_analysis/Numpy/03_numpy_index.py
@@ -3,44 +3,66 @@ import numpy as np
 a = np.array([0,10,20,30,40,50])
 print(a)
 
+print('-'*30)
+
 # 배열 위치 지정
 a1 = a[0]
 print(a1)
+
+print('-'*30)
 
 # 배열의 원소 변경
 a1 = a[5] = 70
 print(a)
 
+print('-'*30)
+
 # 1차원 배열에서 여러 개의 원소 선택
 a1 = a[[1,3,4]]
 print(a1)
+
+print('-'*30)
 
 # 2차원 배열에서 원소 선택
 a1 = np.arange(10,100,10).reshape(3,3)
 print(a1)
 
+print('-'*30)
+
 a2 = a1[0,2] # 행 위치 0, 열 위치 2
 print(a2)
+
+print('-'*30)
 
 # 원소 값 변경
 a2 = a1[2,2] = 95
 print(a1)
 
+print('-'*30)
+
 # 행 전체 가져오기
 a2 = a1[1]
 print(a2)
+
+print('-'*30)
 
 # 특정 행 지정해 전체 값 바꾸기
 a2 = a1[1] = np.array([45,55,65])
 print(a1)
 
+print('-'*30)
+
 # 2차원 배열의 여러 원소 선택하기
 a2 = a1[[0,2],[0,1]]
 print(a2)
 
+print('-'*30)
+
 # 배열에 조건 지정
 a1 = np.array([1,2,3,4,5,6])
 print(a1[a1 > 3])
+
+print('-'*30)
 
 print(a1[(a1 % 2) == 0])
 
--- for_data_analysis/Numpy/04_numpy_slice.py
+++ for_data_analysis/Numpy/04_numpy_slice.py
@@ -5,33 +5,54 @@ b = np.array([0,10,20,30,40,50])
 b1 = b[1:4]
 print(b1)
 
+print('-'*30)
+
 b1 = b[:3]
 print(b1)
 
+print('-'*30)
+
+
 b1 = b[2:]
 print(b1)
+
+print('-'*30)
+
 
 # 원소 변경
 b1 = b[2:5] = np.array([25,35,45])
 print(b1)
 
+print('-'*30)
+
+
 b[3:6] = 60
 print(b)
+
+print('-'*30)
 
 # 2차원 배열의 슬라이싱
 b = np.arange(10,100,10).reshape(3,3)
 print(b)
 
+print('-'*30)
+
 b2 = b[1:3,1:3]
 print(b2)
+
+print('-'*30)
 
 # 일부 생략
 b2 = b[:3,1:]
 print(b2)
 
+print('-'*30)
+
 # 행 지정 열 슬라이싱
 b2 = b[1][0:2]
 print(b2)
+
+print('-'*30)
 
 # 슬라이싱 된 배열에 값 지정
 b[0:2, 1:3] = np.array([[25,35],[55,65]])
