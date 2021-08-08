f3e0fe9f89e0518385e49acd953b165c1828c540
Lee-HB <dkdldspd3@gmail.com>
2021-08-08 22:59:17 +0900

    Python

---
+++ Baekjoon/greedy/greedy_2839.py
@@ -0,0 +1,18 @@
+n = int(input())
+cal = 0
+five = 0
+three = 0
+while n > 0:
+    if n % 5 == 0:
+        five = n//5
+        n = 0
+    else:
+        n -= 3
+        three += 1
+if n < 0:
+    print(-1)
+else:
+    print(five+three)
+
+
+
