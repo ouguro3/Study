3e080798a240752b2350b471ede8760fe17b1816
Lee-HB <dkdldspd3@gmail.com>
2021-11-14 21:26:17 +0900

    Python

---
+++ Baekjoon/String/04_String.py
@@ -0,0 +1,14 @@
+# 문자열 반복
+
+# 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성
+
+n = int(input())
+string = []
+
+for _ in range(n):
+    string.append(input().split())
+
+for x, y in string:
+    for z in range(len(y)):
+        print(y[z]*int(x), end='')
+    print()
