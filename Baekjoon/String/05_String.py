4518e8442ff831143b5cc5619e0d80de262d9c7e
Lee-HB <dkdldspd3@gmail.com>
2021-12-02 20:06:32 +0900

    Python

--- Baekjoon/String/05_String.py
+++ Baekjoon/String/05_String.py
@@ -3,7 +3,26 @@
 # 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성
 
 s = input().lower()
-s_2 = list(set(s))
+s_l = list(set(s))
+cnt = []
+
+for i in s_l:
+    count = s.count(i)
+    cnt.append(count)
+
+if cnt.count(max(cnt)) >= 2:
+    print('?')
+else:
+    print(s_l[(cnt.index(max(cnt)))].upper())
+
+
+
+
+
+
+
+
+
 
 
 
